73be4e72a68e7e539c88d474d8b942471bbe54c9
Yaseen <yaseen.karriem@younglings.africa>
2024-02-22 15:26:25 +0200

    fixed app component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,11 +4,17 @@ import TaskPopUp from "./TaskPopUp.jsx";
 
 function App() {
   return (
-    <>
+    <div className="app">
       <Calendar />
-      <SocialMediaCard />
-      <TaskPopUp />
-    </>
+      <SocialMediaCard name="Instagram" />
+      <SocialMediaCard name="YouTube" />
+      <SocialMediaCard name="Twitter" />
+      {/* Add other social media cards */}
+      <TaskPopUp socialMedia="Instagram" />
+      <TaskPopUp socialMedia="YouTube" />
+      <TaskPopUp socialMedia="Twitter" />
+      {/* Add other task pop-ups */}
+    </div>
   );
 }
 
